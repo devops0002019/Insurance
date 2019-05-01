7643beeee980ca614033305f44a3a9c5ebccc385
Churchill Exotics <churchillexotics@iMac.hsd1.fl.comcast.net>
2019-05-01 16:38:27 -0400

    add a new file

--- js/animated-form.js
+++ js/animated-form.js
@@ -3,15 +3,16 @@ function animatedForm() {
     const arrows = document.querySelectorAll(".enter-next");
 
     arrows.forEach(arrow => {
-
         arrow.addEventListener("click", () => {
 
             const input = arrow.previousElementSibling;
             const parent = arrow.parentElement;
             const nextForm = parent.nextElementSibling;
 
+
             if (input.type === "radio" && validateCoverage(input)) {
                 nextSlide(parent, nextForm);
+
 
             } else if (input.type === "text" && validateUser(input)) {
                 nextSlide(parent, nextForm);
@@ -19,11 +20,15 @@ function animatedForm() {
             } else if (input.type === "email" && validateEmail(input)) {
                 nextSlide(parent, nextForm);
 
+
             } else if (input.type === "password" && validateUser(input)) {
                 nextSlide(parent, nextForm);
 
+
+
             } else if (input.type === "radio" && validateGender(input)) {
                 nextSlide(parent, nextForm);
+
 
             } else {
                 parent.style.animation = "shake 0.5s ease";
@@ -38,6 +43,7 @@ function animatedForm() {
 }
 
 
+
 var progress = document.getElementById('progressBar');
 var statusProgress = document.getElementById('status');
 
@@ -46,17 +52,18 @@ var statusProgress = document.getElementById('status');
 
 function validateCoverage(coverage) {
 
-    var converage = document.getElementsByName('coverage');
+    var coverage = document.getElementsByName('coverage');
 
-    if (!(converage[0].checked || converage[1].checked)) {
+
+    if (!(coverage[0].checked || coverage[1].checked)) {
         error("rgb(101, 109, 255)");
         progress.value = 25;
         statusProgress.innerHTML = `<span class="percentage">25% </span>Completed`;
-        console.log("It's Working well!!!");
+        console.log("coverage is up and running");
         return true;
     } else {
         error("rgb(189, 87, 87)");
-        console.log("It Doesn't Work well!!!");
+        console.log("coverage is not up and running");
         return false;
     }
 }
@@ -66,30 +73,29 @@ function validateCoverage(coverage) {
 function validateUser(user) {
 
     if (user.value.length < 6) {
-        console.log("not enough characters");
         error("rgb(189, 87, 87)");
-        console.log("It Doesn't Work well!!!");
+        console.log("user is not up and running");
         return false;
     } else {
         error("rgb(101, 109, 255)");
         progress.value = 50;
         statusProgress.innerHTML = `<span class="percentage">50% </span>Completed`;
-        console.log("It's Working well!!!");
+        console.log("user is up and running");
         return true;
     }
 }
 
 function validateEmail(email) {
-
     const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     if (validation.test(email.value)) {
         error("rgb(101, 109, 255)");
         progress.value = 75;
         statusProgress.innerHTML = `<span class="percentage">75% </span>Completed`;
+        console.log("email is up and running");
         return true;
     } else {
         error("rgb(189, 87, 87)");
+        console.log("email is not up and running");
         return false;
     }
 }
@@ -99,15 +105,17 @@ function validateEmail(email) {
 
 function validateGender(gender) {
 
-    var gen = document.getElementsByName('gender');
+    var gender = document.getElementsByName('gender');
 
-    if (!(gen[0].checked || gen[1].checked)) {
+    if (!(gender[0].checked || gender[1].checked)) {
         error("rgb(101, 109, 255)");
         progress.value = 100;
         statusProgress.innerHTML = `<span class="percentage">100% </span>Completed`;
+        console.log("gender is up and running");
         return true;
     } else {
         error("rgb(189, 87, 87)");
+        console.log("gender is not up and running");
         return false;
     }
 }
@@ -119,39 +127,86 @@ function validateGender(gender) {
 
 
 
-
-
-    function nextSlide(parent, nextForm) {
-        parent.classList.add('innactive');
-        parent.classList.remove('active');
-        nextForm.classList.add('active');
-    }
-
-    function error(color) {
-        document.body.style.background = color;
-    }
-
-    animatedForm();
-
-    console.log(" hello andrew ");
-
-
-
-
-//} else if (location.className === location.value && validateGender(location)) {
-//    nextSlide(parent, nextForm);
+//    if (genders[i].checked) gerderVal = true;
+//    error("rgb(101, 109, 255)");
+//    i++;
 //
-//    function validateCoverange(converage) {
+//    return true;
+//}
 //
-//        select = document.getElementById('select');
-//        if (select.value) {
+//if (!gerderVal) alert("Must check some option!");
+//error("rgb(189, 87, 87)");
+//return gerderVal;
+//}
+
+
+//if (validation.test(email.value)) {
+//    error("rgb(101, 109, 255)");
+//    return true;
+//} else {
+//    error("rgb(189, 87, 87)");
+//    return false;
+//}
+//}
+
+
+
+
+
+//            function validateGender(gender) {
 //
-//            error("rgb(101, 109, 255)");
-//            return true;
+//                select = document.getElementById('select');
+//                if (select.value) {
 //
-//        } else {
-//            error("rgb(189, 87, 87)");
-//            return false;
-//        }
+//                    error("rgb(101, 109, 255)");
+//                    return true;
+//
+//                } else {
+//                    error("rgb(189, 87, 87)");
+//                    return false;
+//                }
+//            }
+
+
+//function validateCoverange(converage) {
+//
+//    select = document.getElementById('select');
+//    if (select.value) {
+//
+//        error("rgb(101, 109, 255)");
+//        return true;
+//
+//    } else {
+//        error("rgb(189, 87, 87)");
+//        return false;
 //    }
-//
+//}
+
+
+
+
+
+function nextSlide(parent, nextForm) {
+    parent.classList.add('innactive');
+    parent.classList.remove('active');
+    nextForm.classList.add('active');
+}
+
+function error(color) {
+    document.body.style.background = color;
+}
+
+animatedForm();
+
+console.log(" hello andrew ");
+
+
+
+
+
+
+
+
+
+//            } else if (location.className === location.value && validateGender(location)) {
+//                nextSlide(parent, nextForm);
