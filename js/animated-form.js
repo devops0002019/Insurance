5d5c5e3c085a8501db5939f1a3d0d84c699216f1
Churchill Exotics <churchillexotics@iMac.local>
2019-05-02 09:46:02 -0400

    add a new file

--- js/animated-form.js
+++ js/animated-form.js
@@ -45,8 +45,6 @@ var statusProgress = document.getElementById('status');
 
 
 
-
-
 function validateUser(user) {
 
     if (user.value.length < 6) {
@@ -80,9 +78,7 @@ function validateEmail(email) {
 
 
 
-
-
-function validateCoverage(cov) {
+function validateCoverage(coverage) {
 
     var coverage = document.getElementsByName('coverage');
 
@@ -102,7 +98,7 @@ function validateCoverage(cov) {
 
 
 
-function validateGender(gen) {
+function validateGender(gender) {
 
     var gender = document.getElementsByName('gender');
 
@@ -122,69 +118,6 @@ function validateGender(gen) {
 
 
 
-
-
-
-
-//    if (genders[i].checked) gerderVal = true;
-//    error("rgb(101, 109, 255)");
-//    i++;
-//
-//    return true;
-//}
-//
-//if (!gerderVal) alert("Must check some option!");
-//error("rgb(189, 87, 87)");
-//return gerderVal;
-//}
-
-
-//if (validation.test(email.value)) {
-//    error("rgb(101, 109, 255)");
-//    return true;
-//} else {
-//    error("rgb(189, 87, 87)");
-//    return false;
-//}
-//}
-
-
-
-
-
-//            function validateGender(gender) {
-//
-//                select = document.getElementById('select');
-//                if (select.value) {
-//
-//                    error("rgb(101, 109, 255)");
-//                    return true;
-//
-//                } else {
-//                    error("rgb(189, 87, 87)");
-//                    return false;
-//                }
-//            }
-
-
-//function validateCoverange(converage) {
-//
-//    select = document.getElementById('select');
-//    if (select.value) {
-//
-//        error("rgb(101, 109, 255)");
-//        return true;
-//
-//    } else {
-//        error("rgb(189, 87, 87)");
-//        return false;
-//    }
-//}
-
-
-
-
-
 function nextSlide(parent, nextForm) {
     parent.classList.add('innactive');
     parent.classList.remove('active');
@@ -200,12 +133,3 @@ animatedForm();
 console.log(" hello andrew ");
 
 
-
-
-
-
-
-
-
-//            } else if (location.className === location.value && validateGender(location)) {
-//                nextSlide(parent, nextForm);
