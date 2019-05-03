3f9860bfe2f3fc00947c88085131060051e4f0cb
Churchill Exotics <churchillexotics@iMac.local>
2019-05-03 14:46:19 -0400

    add a new file

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ document.documentElement.style .setProperty('--tertiary', '#FAFAFA');
 document.documentElement.style .setProperty('--quaternary', '#0903cf');
 document.documentElement.style .setProperty('--border', '#d8d8d8');
 document.documentElement.style .setProperty('--page-max-width', '1024px');
-document.documentElement.style .setProperty('--navbar-background', '#fff');
+document.documentElement.style .setProperty('--navbar-background', 'var(--white)');
 document.documentElement.style .setProperty('--navbar-text-color', 'var(--primary)');
 document.documentElement.style .setProperty('--header-background', '#fff');
 document.documentElement.style .setProperty('--header-text-color', '#000');
-document.documentElement.style .setProperty('--footer-background', 'var(--secondary)');
+document.documentElement.style .setProperty('--footer-background', 'var(--white)');
 document.documentElement.style .setProperty('--footer-text-color', 'black');
 document.documentElement.style .setProperty('--h1-color', 'black');
 document.documentElement.style .setProperty('--h2-color', '#000');
@@ -18,14 +18,15 @@ document.documentElement.style .setProperty('--seo-page-title-color', 'blue');
 
 const navbar_data = [
    {
+         brand: "Insurance",
    }
 ];
 
 function navbar_template(navbar) {
    return `
     <ul class="menu-list">
-        <li><a href="index.html" class="brand"><h1>Insurance</h1></a></li>
-        <li><a href="index.html" class="brand"><img src="img/logo.png" alt="Logo" height="42" width="42"></a></li>
+        <li><a href="/#" class="brand"><h1>${navbar.brand}</h1></a></li>
+        <li><a href="/#" class="brand"><img src="img/logo.png" alt="Logo" height="42" width="42"></a></li>
     </ul>
  `;
 }
@@ -35,7 +36,7 @@ document.getElementById("navbar-empty").innerHTML = `${navbar_data.map(navbar_te
 
 const footer_data = [
    {
-       brand:   "Insurance Inc",
+       brand: "Insurance",
    }
 ];
 function footer_template(foot) {
@@ -43,7 +44,7 @@ function footer_template(foot) {
     <div class="footer">
      <div class="grid set-cols-1">
        <div class="column">
-        <h2 class="copyright"> Copyright ${foot.brand}. All rights reserved <span id="year"></span> </h2>
+         <h2 class="copyright"> Copyright<a href="/#"> ${foot.brand} </a>. All rights reserved <span id="year"></span> </h2>
        </div>
      </div>
     </div>
